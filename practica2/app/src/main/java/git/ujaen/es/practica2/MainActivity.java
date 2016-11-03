94f2dd7732e2183dd5e52675acedfc19bf19e218
Antonio José León Sánchez <ajls0006@red.ujaen.es>
2016-11-03 11:51:01 +0100

    Cambios para hacer la tarea asincrona en segundo plano

--- practica2/app/src/main/java/git/ujaen/es/practica2/MainActivity.java
+++ practica2/app/src/main/java/git/ujaen/es/practica2/MainActivity.java
@@ -1,10 +1,22 @@
 package git.ujaen.es.practica2;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.SocketAddress;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -34,6 +46,45 @@ public class MainActivity extends AppCompatActivity {
 
         ft.commit();
     }
+    class Autentica{
+        public Autentica(String u, String p, String ip, String port){
+
+        }
+    }
+    class Sesion{
+        public String mSessionId="";
+        public String mExpires="";
+    }
+    class Autentication extends AsyncTask<Autentica,Void,Sesion>{
+
+        @Override
+        protected void onPreExecute() {
+            Autentica datos = null;
+            super.onPreExecute();
+        }
+
+        @Override
+        protected Sesion doInBackground(Autentica... params) {
+            try {
+                Socket s = new Socket (InetAddress.getByName("hhtp://www4.ujaen.es"),80);
+                PrintWriter pw = new PrintWriter(s.getOutputStream());
+                pw.print("GET /~jccuevas/ssmm/autentica.php?user=user&pass=12345 HTTP/1.1\r\nHost: www4.ujaen.es \r\n\r\n");
+                BufferedReader br = new BufferedReader(new InputStreamReader(s.getInputStream()));
+                String t;
+                while((t = br.readLine()) != null){
+                    Log.d("Prueba",t);
+                }
+                br.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+
+    }
+
+
 
 
 }
